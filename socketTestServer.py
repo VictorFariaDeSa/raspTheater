8a64b2c5c9b4653dd91efcf9e6a8f01f0b82d862
Victor Faria de Sá <savictor3963@gmail.com>
2025-01-09 12:40:14 -0300

    SocketServerCreation

--- socketTestServer.py
+++ socketTestServer.py
@@ -1,5 +1,5 @@
 from flask import Flask,request,jsonify,Response,abort
-from flask_socketio import SocketIO,emit
+from flask_socketio import SocketIO,emit,send
 import json
 import os
 
@@ -7,49 +7,49 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "123"
 socketio = SocketIO(app)
 
+connected_users = {}
+
 with open("videos.json","r") as file:
     video_dict = json.load(file)
 
+@socketio.on("connect")
+def handle_connect():
+    sid = request.sid
+    print(f"Usuário conectado: {sid}")
+
+@socketio.on("register_user")
+def handle_register_user(data):
+    sid = request.sid
+    username = data.get("user", "Usuário Anônimo")
+    connected_users[sid] = username
+    print(f"Usuário registrado: {username} (sid: {sid})")
+
+@socketio.on("disconnect")
+def handle_disconnect():
+    sid = request.sid
+    connected_users.pop(sid, None)
+    print(f"Usuário desconectado: {sid}")
+
+
 @app.route("/",methods=["POST","GET"])
 def home():
-    pass 
-
-
-
-
-
+    socketio.emit("test_message", {"message": "Nova Requisição realizada."})
+    return "ok" ,200
 
 @app.route("/get_video_dict")
 def get_video():
     return video_dict,200
 
-
-@app.route("/upload_file", methods=["POST"])
-def upload_file():
-    print(request.files)
-    try:
-        if 'file' not in request.files:
-            return jsonify({"error": "No file part"}), 400
-        
-        file = request.files['file']
-        if file.filename == '':
-            return jsonify({"error": "No selected file"}), 400
-        upload_folder = "Videos"
-        filename = file.filename
-        file.save(os.path.join(upload_folder, filename))
-
-        return jsonify(video_dict), 200
-
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+@app.route("/update_video/",methods=["POST"])
+def update_class():
+    data = request.get_json()
+    video_dict.update(data)
     
-  
-
-@app.route("/update_video/<raspberry_id>/<new_video_url>")
-def update_video(raspberry_id, new_video_url):
-    video_dict[raspberry_id] = new_video_url
-    socketio.emit('video_update', {raspberry_id: new_video_url})
-    return "Updated", 200
+    json_object = json.dumps(video_dict, indent=4)
+    with open("videos.json", "w") as outfile:
+        outfile.write(json_object)
+    socketio.emit("update_videos_dict",video_dict)
+    return jsonify(video_dict)
 
 @app.route('/video-stream/<videoName>')
 def serve_video_stream(videoName):
@@ -80,6 +80,25 @@ def serve_video_stream(videoName):
     except Exception as e:
         abort(500, f'Erro ao processar o vídeo: {str(e)}')
 
+@app.route("/upload_file", methods=["POST"])
+def upload_file():
+    print(request.files)
+    try:
+        if 'file' not in request.files:
+            return jsonify({"error": "No file part"}), 400
+        
+        file = request.files['file']
+        if file.filename == '':
+            return jsonify({"error": "No selected file"}), 400
+        upload_folder = "Videos"
+        filename = file.filename
+        file.save(os.path.join(upload_folder, filename))
+
+        return jsonify(video_dict), 200
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 @app.route("/delete_video/<videoName>", methods=["DELETE"])
 def delete_video(videoName):
     try:
@@ -93,4 +112,4 @@ def delete_video(videoName):
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    socketio.run(app)
